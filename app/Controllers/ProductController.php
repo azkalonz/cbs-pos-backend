b51ac26537b33505e8c115854b532554809abe4f
Mark Joseph Judaya <aeartixenter@gmail.com>
2020-11-10 22:05:03 +0800

    fix:products without cost history doesn't show

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -10,25 +10,12 @@ class ProductController
 {
     public function all(Request $request, Response $response)
     {
-        $products = DB::select("SELECT * FROM (products 
-        INNER JOIN product_cost_history 
-        ON products.product_id = product_cost_history.product_id)
-        INNER JOIN product_prices ON products.product_id = product_prices.product_id 
+        $products = DB::select("select * from products INNER JOIN product_prices ON products.product_id = product_prices.product_id 
         WHERE products.invisible != 1
         AND products.product_status_id = 1
-        AND product_cost_history.date_time = (
-            SELECT MAX(date_time)
-            FROM product_cost_history
-            WHERE product_id = products.product_id
-          )
         GROUP BY products.product_id
-        ORDER BY products.product_name ASC");
-        // $products = Product::join('product_prices', 'product_prices.product_id', '=', 'products.product_id')
-        //     ->where('products.invisible', '!=', '1')
-        //     ->where('products.product_status_id', '=', '1')
-        //     ->groupBy('products.product_id')
-        //     ->orderBy('products.product_name', 'ASC')
-        //     ->get();
+        ORDER BY products.product_id ASC");
+
         if (!empty($_GET['search'])) {
             $products = Product::where('product_name', 'LIKE', '%' . $_GET['search'] . '%')
                         ->where('products.invisible','!=','1')
