bc167c2a53243c4a1631844eb21690c68698fd4f
azkalonz <aeartixenter@gmail.com>
2020-11-30 17:01:28 +0800

    post backup

--- app/Routes/Backup.php
+++ app/Routes/Backup.php
@@ -3,7 +3,7 @@ namespace App\Routes;
 
 class Backup {
     function __construct($app) {
-        $app->get('/backup', '\App\Controllers\BackupController:backup');
+        $app->post('/backup', '\App\Controllers\BackupController:backup');
         $app->post('/restore', '\App\Controllers\BackupController:restore');
     }
 }
